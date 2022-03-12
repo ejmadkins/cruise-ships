ff671192c3631c082719870dbf24e841494fd9bf
Ed Adkins <ejmadkins@gmail.com>
2022-03-12 20:20:17 +0000

    add test for ships to set sail

--- __test__/cruise-ship.test.js
+++ __test__/cruise-ship.test.js
@@ -1,13 +1,19 @@
 const CruiseShip = require("../src/cruise-ship");
 
 describe("starting point", () => {
-  const cruiseShip = new CruiseShip("Manchester");
-
   it("can be instantiated", () => {
+    const cruiseShip = new CruiseShip("Manchester");
     expect(cruiseShip).toBeInstanceOf(Object);
   });
 
   it("define a starting point for the cuise", () => {
+    const cruiseShip = new CruiseShip("Manchester");
     expect(cruiseShip.start).toBe("Manchester");
+  });
+
+  it("can set sail", () => {
+    const cruiseShip = new CruiseShip("Manchester");
+    cruiseShip.setSail();
+    expect(cruiseShip.start).toBeFalsy();
   });
 });
--- src/cruise-ship.js
+++ src/cruise-ship.js
@@ -3,7 +3,12 @@ function CruiseShip(start) {
   this.start = start;
 }
 
+CruiseShip.prototype.setSail = function () {
+  this.start = null;
+};
+
 const manchesterCruise = new CruiseShip("Manchester");
 console.log(manchesterCruise.start);
+console.log((manchesterCruise.setSail = "london"));
 
 module.exports = CruiseShip;
