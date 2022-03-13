7ecc6c9561b61bbf1d59c740222eae92dbdb802c
Ed Adkins <ejmadkins@gmail.com>
2022-03-13 08:45:15 +0000

    add dock

--- src/cruise-ship.js
+++ src/cruise-ship.js
@@ -1,14 +1,20 @@
 // I want a ship to have a starting port.
 function CruiseShip(start) {
   this.start = start;
+  this.destinationPort = "";
 }
 
 CruiseShip.prototype.setSail = function () {
   this.start = null;
 };
 
-const manchesterCruise = new CruiseShip("Manchester");
-console.log(manchesterCruise.start);
-console.log((manchesterCruise.setSail = "london"));
+CruiseShip.prototype.dock = function (destinationPort) {
+  this.destinationPort = destinationPort;
+};
+
+const cruise = new CruiseShip("Liverpool");
+console.log(cruise.start);
+cruise.dock("St. Martin");
+console.log(cruise.destinationPort);
 
 module.exports = CruiseShip;
